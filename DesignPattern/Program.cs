461859617ff3f9cb41f50398dc55b454d1ec6bea
balaweblog <Balamurugan.Alagumalai@gmail.com>
2017-12-14 10:57:30 +0530

    lk

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -56,11 +57,68 @@ namespace DesignPattern
             */
 
             //further move on go with lazy loading after invoking
-
+            /*
             Parallel.Invoke(
             () => EagerLoading.DomyWorkonceInlifetime(),
             () => EagerLoading.DomyWorkonceInlifetime());
-            Console.Read();
+            */
+
+            /* practical logging in exception logging
+            
+             */
+          
+        //inside ur constructor
+       
+        
+        //Console.Read();
+    }
+    }
+    public class Controllers
+    {
+        private ILog _Ilog;
+        public Controller()
+        {
+            _Ilog = Log.GetInstance;
+        }
+        //oneexception
+        _ILog.LogException(filterContext.Exception.ToString());
+            filterContext.ExceptionHandled = true;
+            this.View("Error").ExecuteResult(this.ControllerContext);
+
+    }
+    public interface ILog
+    {
+        void LogException(string message);
+    }
+    public sealed class Log:ILog
+    {
+        private static readonly Lazy<Log> instance = new Lazy<Log>(() => new Log());
+
+        public static Log GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+        private Log()
+        {
+
+        }
+
+        public void LogException(string message)
+        {
+            string fileName = string.Format("{0}_{1}.log", "Exception", DateTime.Now.ToShortDateString());
+            string logFilePath = string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("----------------------------------------");
+            sb.AppendLine(DateTime.Now.ToString());
+            sb.AppendLine(message);
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.Write(sb.ToString());
+                writer.Flush();
+            }
         }
     }
     public static class StaticClasses
